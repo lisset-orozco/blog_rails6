9b2c12a3bdd308d2a6f8759bd03b17f332f15d64
lisset-orozco <liss612@gmail.com>
2019-05-25 16:31:09 -0500

    create articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# articles - blog
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      render json: @article
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title)
+  end
+end
