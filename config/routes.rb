3f453df49f2a422b4dfee1971c4f3a5a5fba5318
lisset-orozco <liss612@gmail.com>
2019-05-25 23:48:29 -0500

    render partial nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  get 'articles/new', to: 'articles#new'
+  get 'articles/new', to: 'articles#new', as: :new_article
   get 'articles/:id', to: 'articles#show'
   get 'articles/:id/edit', to: 'articles#edit'
   get 'articles', to: 'articles#index'
