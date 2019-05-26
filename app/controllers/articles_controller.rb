b4094db383256dfbdab68aa837679c83a8be8715
lisset-orozco <liss612@gmail.com>
2019-05-26 01:37:33 -0500

    protect routes with login

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,7 @@
 # articles - blog
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
 
   def index
     @articles = Article.all
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
+# home blog
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   def index
   end
 end
