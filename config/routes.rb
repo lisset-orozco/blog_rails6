749e9b3b94b215361ee65571c661373adf4de5de
lisset-orozco <liss612@gmail.com>
2019-05-26 16:28:36 -0500

    best practices

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  get 'articles/user/:user_id', to: 'articles#from_author'
+  resources :articles do
+    # get 'articles/user/:user_id', to: 'articles#from_author'
+    get 'user/:user_id', to: 'articles#from_author', on: :collection
 
-  get 'articles/new', to: 'articles#new', as: :new_article
-  get 'articles/:id', to: 'articles#show'
-  get 'articles/:id/edit', to: 'articles#edit'
-  get 'articles', to: 'articles#index'
+    # collection do
+    #   get 'user/:user_id', to: 'articles#from_author'
+    # end
+  end
 
-  post 'articles', to: 'articles#create'
-  patch 'articles/:id', to: 'articles#update', as: :article
-  delete 'articles/:id', to: 'articles#destroy'
-  # resources :articles
+  # get 'articles/new', to: 'articles#new', as: :new_article
+  # get 'articles/:id', to: 'articles#show'
+  # get 'articles/:id/edit', to: 'articles#edit'
+  # get 'articles', to: 'articles#index'
+
+  # post 'articles', to: 'articles#create'
+  # patch 'articles/:id', to: 'articles#update', as: :article
+  # delete 'articles/:id', to: 'articles#destroy'
 end
