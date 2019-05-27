1d6426a03f7ecb0c3d2f1a3dd461190d07c9f905
lisset-orozco <liss612@gmail.com>
2019-05-26 19:08:16 -0500

    upload and show avatar_user with active_storage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :categories
-  devise_for :users
+  get 'users/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  devise_for :users
 
+  root 'home#index'
+  get 'profile', to: 'users#edit'
+
+  resources :users, only: :update
+  resources :categories
   resources :articles do
     # get 'articles/user/:user_id', to: 'articles#from_author'
     get 'user/:user_id', to: 'articles#from_author', on: :collection
